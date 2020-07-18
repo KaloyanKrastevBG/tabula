1e6ffb35d3a657dd923ffb2a8d963dd619aa1345
KaloyanKrastevBG <50043391+KaloyanKrastevBG@users.noreply.github.com>
2020-07-18 09:20:14 +0300

    UserEntity, RoleEntity added

---
+++ src/main/java/bg/softuni/tabula/users/model/RoleEntity.java
@@ -0,0 +1,16 @@
+package bg.softuni.tabula.users.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+}
