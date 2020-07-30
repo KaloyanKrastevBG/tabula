39644a05f5fdd35e397bbec0c6b23209ad82cb6d
KaloyanKrastevBG <50043391+KaloyanKrastevBG@users.noreply.github.com>
2020-07-30 12:44:54 +0300

    Delete Announcement is implemented

--- src/main/java/bg/softuni/tabula/announcement/service/AnnouncementService.java
+++ src/main/java/bg/softuni/tabula/announcement/service/AnnouncementService.java
@@ -47,5 +47,10 @@ public class AnnouncementService {
 
     }
 
+    public void delete(Long announcementId){
+        announcementRepository.deleteById(announcementId);
+
+    }
+
     
 }
--- src/main/java/bg/softuni/tabula/announcement/web/AnnouncementController.java
+++ src/main/java/bg/softuni/tabula/announcement/web/AnnouncementController.java
@@ -7,10 +7,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -55,12 +52,18 @@ public class AnnouncementController {
             return "redirect:/announcements/new";
 
         }
-
         announcementService.createOrUpdateAnnouncement(announcementDTO);
 
         return "redirect:/announcements";
 
 
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/delete")
+    public String delete(@ModelAttribute(name="deleteId") Long deleteId){
+        announcementService.delete(deleteId);
+        return "redirect:/announcements";
     }
 
 
